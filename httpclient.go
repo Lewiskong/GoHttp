316a8ceb255820868d31db26d23824d4d4ddfed7
孔令玄 <847496788@qq.com>
2017-08-29 01:57:25 +0800

    init commit

---
+++ httpclient.go
@@ -0,0 +1,249 @@
+package httpclient
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"net/http"
+	"net/url"
+	"reflect"
+	"strings"
+)
+
+/**
+http.get("/fdfdfasdf",func(req,rsp){
+
+})
+
+	err:=http.get("fdsafdsa")
+	.addHeader("","")
+	.addCookie("","")
+	.getJson(&msg)
+	.execute()
+
+http.get("",func(){
+
+})
+*/
+
+type HttpMethod string
+
+const (
+	httpGet  HttpMethod = "GET"
+	httpPost HttpMethod = "POST"
+)
+
+type HttpClient struct {
+	Error    error
+	Response *http.Response
+
+	tasks   map[*interface{}]string
+	method  HttpMethod
+	request *http.Request
+}
+
+func Get(rawurl string) *HttpClient {
+	client := CreateDefault()
+	client.method = httpGet
+
+	client.request, client.Error = http.NewRequest("GET", "", nil)
+	if client.Error != nil {
+		return client
+	}
+
+	return client.handle(rawurl)
+}
+
+func Post(rawurl string) *HttpClient {
+	client := CreateDefault()
+	client.method = httpPost
+	client.request, client.Error = http.NewRequest("POST", "", nil)
+	if client.Error != nil {
+		return client
+	}
+
+	return client.handle(rawurl)
+}
+
+func CreateDefault() *HttpClient {
+	client := new(HttpClient)
+	client.tasks = map[*interface{}]string{}
+	return client
+}
+
+func (client *HttpClient) handle(rawurl string) *HttpClient {
+
+	if client.Error != nil {
+		return client
+	}
+
+	client.request.URL, client.Error = url.ParseRequestURI(rawurl)
+	if client.Error != nil {
+		return client
+	}
+	// fmt.Println(client.request.URL)
+	return client
+}
+
+func (client *HttpClient) ContentType(contentType string) *HttpClient {
+	if client.Error != nil {
+		return client
+	}
+
+	client.request.Header.Set("Content-Type", contentType)
+
+	return client
+}
+
+func (client *HttpClient) Body(body io.Reader) *HttpClient {
+	if client.Error != nil {
+		return client
+	}
+
+	client.request.Body = ioutil.NopCloser(body)
+
+	return client
+}
+
+func (client *HttpClient) AddHeader(key, value string) *HttpClient {
+	if client.Error != nil {
+		return client
+	}
+
+	client.request.Header.Add(key, value)
+
+	return client
+}
+
+func (client *HttpClient) AddCookie(name, value string) *HttpClient {
+	if client.Error != nil {
+		return client
+	}
+
+	ck := new(http.Cookie)
+	ck.Name = name
+	ck.Value = value
+
+	client.request.AddCookie(ck)
+
+	return client
+}
+
+func (client *HttpClient) GetString(v interface{}) *HttpClient {
+	if client.Error != nil {
+		return client
+	}
+	client.tasks[&v] = "string"
+	return client
+}
+
+func (client *HttpClient) GetJson(v interface{}) *HttpClient {
+	if client.Error != nil {
+		return client
+	}
+	client.tasks[&v] = "json"
+	return client
+}
+
+func (client *HttpClient) GetJsonp(v interface{}) *HttpClient {
+	if client.Error != nil {
+		return client
+	}
+	client.tasks[&v] = "jsonp"
+	return client
+}
+
+func (client *HttpClient) GetJce(v interface{}) *HttpClient {
+	if client.Error != nil {
+		return client
+	}
+	client.tasks[&v] = "jce"
+	return client
+}
+
+func (client *HttpClient) Execute() (err error) {
+	if client.Error != nil {
+		return client.Error
+	}
+
+	c := http.DefaultClient
+	rsp, err := c.Do(client.request)
+
+	if err != nil {
+		return err
+	}
+	content, err := ioutil.ReadAll(rsp.Body)
+	if err != nil {
+		return err
+	}
+	//debug
+	// fmt.Println(string(content[:]))
+	for value, tp := range client.tasks {
+		switch tp {
+		case "string":
+			err := setString(value, content)
+			if err != nil {
+				return err
+			}
+
+		case "json":
+			err := setJSON(value, content)
+			if err != nil {
+				return err
+			}
+
+		case "jsonp":
+			err := setJSONP(value, content)
+			if err != nil {
+				return err
+			}
+
+		case "jce":
+
+		default:
+		}
+	}
+	if err != nil {
+		return err
+	}
+	//debug
+	// fmt.Println(string(content[:]))
+	return nil
+}
+
+func setString(v *interface{}, content []byte) error {
+	interf := *v
+	vtype := reflect.TypeOf(interf)
+	vvalue := reflect.ValueOf(interf)
+
+	if vtype.Kind() != reflect.Ptr {
+		return fmt.Errorf("error happened when parse json : the param obj %s must be pointer", vtype.Kind().String())
+	}
+	vvalue = reflect.Indirect(vvalue)
+	vvalue.SetString(string(content[:]))
+	return nil
+}
+
+// SetJSON hello
+func setJSON(v *interface{}, content []byte) error {
+	interf := *v
+	err := json.Unmarshal(content, interf)
+	return err
+}
+
+func setJSONP(v *interface{}, content []byte) error {
+	interf := *v
+	str := string(content[:])
+	start := strings.Index(str, "{")
+	end := strings.LastIndex(str, "}")
+	if start < 0 || end < 0 {
+		return fmt.Errorf("Parse jsonp error , wrong jsonp format : %s ", str)
+	}
+	err := json.Unmarshal(content[start:end+1], interf)
+	return err
+}
+
+func setJce(v *interface{}) error {
+	return nil
+}
---
+++ test/httpclient_test.go
@@ -0,0 +1,132 @@
+package main
+
+import (
+	".."
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"strings"
+	"testing"
+)
+
+func TestGetMethod(t *testing.T) {
+	err :=
+		httpclient.Get("https://www.baidu.com").
+			Execute()
+	if err != nil {
+		t.Fail()
+	}
+}
+
+func TestEverything(t *testing.T) {
+	// rsp, err := http.Get("https://www.okcoin.cn/api/v1/ticker.do?symbol=btc_cny")
+	req, _ := http.NewRequest("GET", "https://www.okcoin.cn/api/v1/ticker.do?symbol=btc_cny", nil)
+	rsp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		fmt.Println(err)
+	}
+	bts, _ := ioutil.ReadAll(rsp.Body)
+	fmt.Println(string(bts[:]))
+}
+
+func TestGetString(t *testing.T) {
+	str := ""
+	err :=
+		httpclient.Get("https://www.okcoin.cn/api/v1/ticker.do?symbol=btc_cny").
+			GetString(&str).
+			Execute()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(str)
+}
+
+/**
+ *
+ * 测试GetJson
+ *
+ */
+func TestGetJson(t *testing.T) {
+	obj := JsonObject{}
+	err :=
+		httpclient.Get("https://www.okcoin.cn/api/v1/ticker.do?symbol=btc_cny").
+			GetJson(&obj).
+			Execute()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(obj)
+}
+func TestGetJsonp(t *testing.T) {
+	obj := JsonpObject{}
+	err :=
+		httpclient.Get("http://10.240.109.25:8080/upload/reportasnytasks?type=41&vid=j05133ah8u0&format=320088&ip=10.177.143.50&otype=json").
+			GetJsonp(&obj).
+			Execute()
+	if err != nil {
+		fmt.Println(err)
+		t.Fail()
+	}
+	fmt.Println(obj)
+}
+
+func TestMixed(t *testing.T) {
+	obj := JsonObject{}
+	str := ""
+	err :=
+		httpclient.Get("https://www.okcoin.cn/api/v1/ticker.do?symbol=btc_cny").
+			GetJson(&obj).
+			GetString(&str).
+			Execute()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(obj)
+	fmt.Println(str)
+}
+
+func TestHeaderAndCookie(t *testing.T) {
+	err :=
+		httpclient.Post("http://localhost:8888/upload").
+			ContentType("application/json").
+			Body(strings.NewReader("{\"a\":1}")).
+			AddCookie("name", "lewiskong").
+			AddHeader("from", "localhost").
+			Execute()
+	if err != nil {
+		panic(err)
+		t.Fail()
+	}
+
+}
+
+func TestWrongUrl(t *testing.T) {
+	err :=
+		httpclient.Post("http://localhost:1231/nil").
+			Execute()
+	if err != nil {
+		panic(err)
+		t.Fail()
+	}
+}
+
+type JsonObject struct {
+	Date   string     `json:"date"`
+	Ticker TickerItem `json:"ticker"`
+}
+type TickerItem struct {
+	Low  string `json:"low"`
+	Sell string `json:"sell"`
+	Vol  string `json:"vol"`
+	Buy  string `json:"buy"`
+	High string `json:"high"`
+	Last string `json:"last"`
+}
+
+type JsonpObject struct {
+	Result ResultItem `json:"result"`
+}
+type ResultItem struct {
+	Code float64 `json:"code"`
+	Msg  string  `json:"msg"`
+}
